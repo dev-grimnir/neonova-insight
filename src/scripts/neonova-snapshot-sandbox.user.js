85a5357deb755d2fc8b60d30fa94c85c6e963764
dev-grimnir <dev.grimnir@protonmail.com>
2026-04-03 12:27:26 -0400

    Add NovaSubscriber user script for dashboard
    This user script provides a real-time customer modem connection dashboard for Neonova, including necessary dependencies.

---
+++ src/scripts/neonova-snapshot-sandbox.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         NovaSubscriber - Dashboard
+// @namespace    http://tampermonkey.net/
+// @version      29.76
+// @description  Real-time customer modem connection dashboard (separate script)
+// @author       dev-grimnir
+// @match        https://admin.neonova.net/*
+// @grant        none
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/scripts/neonova-snapshot-sandbox.user.js
+// @downloadURL  https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/scripts/neonova-snapshot-sandbox.user.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/views/base-neonova-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/views/neonova-base-modal-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/views/neonova-snapshot-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/controllers/neonova-snapshot-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/models/neonova-snapshot-model.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/controllers/neonova-http-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/core/utils.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/controllers/neonova-collector.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/dev/src/controllers/neonova-analyzer.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Only run in the MAIN content frame
+    if (window.name !== 'MAIN') {
+        return;
+    }
+
+    (async () => {
+        const dashboardController = new NeonovaDashboardController();  
+    })();
+})();
+
