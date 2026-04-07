51c441a7b4e9b0d17e20c7722a45680ad8a18a33
dev-grimnir <dev.grimnir@protonmail.com>
2026-04-07 19:57:11 -0400

    Update neonova-add-customer-controller.js

--- src/controllers/neonova-add-customer-controller.js
+++ src/controllers/neonova-add-customer-controller.js
@@ -1,6 +1,7 @@
 class NeonovaAddCustomerController {
-    constructor(dashboardController) {
-        this.dashboardController = dashboardController;
+    #tabController
+    constructor(tabController) {
+        this.#tabController = tabController;
         this.view = new NeonovaAddCustomerView(this);
     }
 
@@ -15,7 +16,7 @@ class NeonovaAddCustomerController {
         if (!un) return;
     
         try {
-            this.dashboardController.add(un, friendlyName);
+            this.#tabController.add(un, friendlyName);
         } catch (err) {
             this.view.showError("Failed to add customer: " + err.message);
             return;
