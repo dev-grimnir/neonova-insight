f4833a56abb6eb2617f4d5e1d2975a2543f90722
dev-grimnir <dev.grimnir@protonmail.com>
2026-04-30 18:11:20 -0400

    Update neonova-admin-manager-model.js

--- src/models/neonova-admin-manager-model.js
+++ src/models/neonova-admin-manager-model.js
@@ -9,19 +9,6 @@ class NeonovaAdminManagerModel {
         }
     }
 
-    getAdmin(name) {
-        return this.admins.find(a => a.name === name);
-    }
-
-    addOrUpdateAdmin(adminData) {
-        const idx = this.admins.findIndex(a => a.name === adminData.name);
-        if (idx >= 0) {
-            this.admins[idx] = { ...this.admins[idx], ...adminData };
-        } else {
-            this.admins.push({ name: adminData.name, phoneNumber: adminData.phoneNumber });
-        }
-    }
-
     removeAdmin(name) {
         this.admins = this.admins.filter(a => a.name !== name);
     }
@@ -30,7 +17,7 @@ class NeonovaAdminManagerModel {
         return this.admins.find(a => a.name === name);
     }
 
-    getAdminsArray() {
-        return [...this.admins];
+    toJSON() {
+        return this.admins.map(a => a.toJSON());
     }
 }
